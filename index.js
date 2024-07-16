a08210124e7b5e2cd5ba5a4e14837b09be5a9ed2
misbah-aslam16 <misbahaslam000@gmail.com>
2024-07-16 12:47:50 -0700

    new file added

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 //--------------------QuUESTION 01--------------------//
 var now = new Date();
-document.write(now) + "<br>";
+document.write(now + "<br>");
 
 //--------------------QuUESTION 02--------------------//
 var months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
@@ -21,38 +21,31 @@ if(currentDay==0 || currentDay==6){
 }
  
 //--------------------QuUESTION 05--------------------//
-var now=new Date()
-var date=now.getDate()
+var now=new Date();
+var date=now.getDate();
 
 if(date >=15){
-  document.write("First fifteen days of the month.<br>")  
+  document.write("First fifteen days of the month.<br>") ; 
 }
 else{
-    document.write("Last days of the month.<br>")
+    document.write("Last days of the month.<br>");
 }
 //--------------------QuUESTION 06--------------------//
-// Write a program that determines the minutes since
-// midnight, Jan. 1, 1970 and assigns it to a variable that
-// hasn't been declared beforehand. Use any variable you like
-// to represent the Date object.
-// Create a Date object representing the current date and time
-let now1 = new Date();
-document.write("current date "+now1)
-let millisecondsSinceEpoch = now1.getTime();
-document.write(" Elapsed Milliseconds since january 1,1970" + millisecondsSinceEpoch)
-let minutesSinceEpoch = Math.floor(millisecondsSinceEpoch / (1000 * 60));
+var now1 = new Date();
+document.write("current date " + now1 + "<br>");
+var millisecondsSinceEpoch = now1.getTime();
+document.write(" Elapsed Milliseconds since january 1,1970" + millisecondsSinceEpoch + "<br>");
+var minutesSinceEpoch = Math.floor(millisecondsSinceEpoch / (1000 * 60));
 document.write("Elapsed Minute since january 1,1970" + minutesSinceEpoch + "<br>");
 
 //--------------------QuUESTION 07--------------------//
-// Write a program that tests whether it's before noon and
-// alert “Its AM” else “its PM”.
-let now2=new Date()
+var now2=new Date();
 var hours=now2.getHours()
 if(hours <=11){
-    document.write("Its AM<br>")
+    document.write("Its AM <br>");
 }
 else{
-    document.write("Its PM<br>")
+    document.write("Its PM <br>");
 }
 
 //--------------------QuUESTION 08--------------------// 
@@ -60,13 +53,10 @@ let laterDate = new Date(2020, 11, 31);
 document.write("Later Date" + laterDate + "<br>");
 
 //--------------------QuUESTION 09--------------------//
-// Create a date object of the starting date of this Ramadan
-// and alert the number of days past since 1st Ramadan?
-// Note: 1st Ramadan was on June 18, 2015
-let now3=new Date()
-let ramzan=new Date(2015 ,6,18)
+let now3=new Date();
+let ramzan=new Date(2015 ,6,18);
 var out=now3-ramzan;
 let daysDifference = Math.floor(out / (1000 * 60 * 60 * 24));
-document.write("Time is" + daysDifference + "<br>")
+document.write("Time is" + daysDifference + "<br>");
 
 //--------------------QuUESTION 10--------------------//
